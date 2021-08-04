a0dec659728765d7b83d1c9ca3accc14311826b8
Glitch (starter-cra-and-express) <none>
2021-08-04 17:16:17 +0700

    final commit

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 const app = express();
 
+const movies_metadata = require(path.join(__dirname,"movies_metadata.json"))
+
 // PWAs want HTTPS!
 function checkHttps(request, response, next) {
   // Check the protocol — if http, redirect to https.
-  if (request.get("X-Forwarded-Proto").indexOf("https") != -1) {
+  if (request.get("X-Forwarded-Proto")?.indexOf("https") != -1) {
     return next();
   } else {
     response.redirect("https://" + request.hostname + request.url);
@@ -14,6 +16,7 @@ function checkHttps(request, response, next) {
 }
 
 app.all("*", checkHttps);
+app.use(express.json())
 
 // A test route to make sure the server is up.
 app.get("/api/ping", (request, response) => {
@@ -24,8 +27,31 @@ app.get("/api/ping", (request, response) => {
 // A mock route to return some data.
 app.get("/api/movies", (request, response) => {
   console.log("❇️ Received GET request to /api/movies");
-  response.json({ data: [{ id: 1, name: '1' }, { id: 2, name: '2' }] });
+
+  response.json({
+    data: movies_metadata
+  });
 });
+
+// Route to get movie per id
+app.get("/api/movies/:id", (request, response) => {
+  let movieId = +request.params.id,
+      selectedMovie
+  
+  console.log(`❇️ Received GET request to /api/movies/:id with id=  ${movieId}`)
+  
+  movies_metadata.every(movie => {
+    if(movie.id === movieId){
+      selectedMovie = movie
+      return false
+    }
+    return true    
+  })
+  
+  response.json({
+    data: selectedMovie
+  })
+})
 
 // Express port-switching logic
 let port;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,  } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/movie/:id">
+            <DetailMovie />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+function Home(){
+
   const [movies, setMovies] = useState([]);
+
   useEffect(() => {
     async function getData() {
       const response = await fetch('/api/movies');
@@ -12,27 +36,69 @@ function App() {
     }
     getData();
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and your changes will live-update automatically.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>Nice Movies:</p>
-        <p>{JSON.stringify(movies)}</p>
+        <div className="movie-grid">
+          {
+            movies.map(movie => {
+              return <MovieCard  movie={movie}/>
+            })
+          }
+        </div>
         
       </header>
     </div>
   );
+}
+
+function MovieCard(props){
+  const movie = props.movie
+
+  return (
+    <Link to={`/movie/${movie.id}`}>
+      <div>
+        title: {movie.original_title}
+        tagline: {movie.tagline}
+        vote_average: {movie.vote_average}
+      </div>
+    </Link>
+  )
+}
+
+function DetailMovie(){
+  const [movie, setMovie] = useState({});
+  const params = useParams()
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`/api/movies/${params.id}`);
+      const payload = await response.json();
+      payload.data.release_date = new Date(payload.data.release_date.split('/').reverse().join('/')).toLocaleDateString(undefined,{});
+      setMovie(payload.data);
+    }
+    getData();
+  }, []);
+
+  return(
+    <div>
+      <Link to="/">back to home</Link>
+      <ul>
+        <li>original title: {movie.original_title}</li>
+        <li>overview: {movie.overview}</li>
+        <li>release date: {movie.release_date}</li>
+        <li>runtime: {movie.runtime} Minutes</li>
+        <li>status: {movie.status}</li>
+        <li>tagline: {movie.tagline}</li>
+        <li>title: {movie.title}</li>
+        <li>vote average: {movie.vote_average}</li>
+        <li>vote count: {movie.vote_count}</li>
+      </ul>
+    </div>
+  )
+
 }
 
 export default App;
